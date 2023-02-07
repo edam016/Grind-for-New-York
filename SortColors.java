7a80bd55873247d5ffb4510950761285da7102b7
Eric Dam <edam3268@gmail.com>
2023-02-07 01:09:27 -0500

    Solved 1 med q

---
+++ SortColors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int num : nums){
+            if(num == 0) count0++;
+            if(num == 1) count1++;
+            if(num == 2) count2++;
+        }
+        for(int i = 0;i<=nums.length-1;i++){
+            if(i < count0) nums[i] = 0;
+            else if(i >= count0 && i< count0+count1) nums[i] = 1;
+            else nums[i] = 2;
+         }
+    }
+}
