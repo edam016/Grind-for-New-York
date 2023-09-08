f3fab74760c791f03c59764db21d99cb5913476d
Eric Dam <edam3268@gmail.com>
2023-09-07 20:07:53 -0400

    Added 1 LC medium

---
+++ MinimumInRotatedSortedArray.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start < end){
+            int middle = (start + end)/2;
+            if(middle != 0 && nums[middle] < nums[middle - 1]){
+                return nums[middle];
+            }
+            else if(nums[middle] > nums[end]){
+                start = middle + 1;
+            }
+            else{
+                end = middle - 1;
+            }
+        }
+        return nums[start];
+    }
+} {
+    
+}
