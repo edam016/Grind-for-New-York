c0a52ce034f4c91944dd9c5e172b1f1ba0733c10
Eric Dam <edam3268@gmail.com>
2023-09-24 23:42:30 -0400

    Edit Majority Element Q

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,20 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap();
-        int result = 0;
-        for(int i = 0;i<=nums.length-1;i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i], hm.get(nums[i]) + 1);
+        int num = nums[0];
+        int count = 1;
+        for(int i = 1;i<=nums.length-1;i++){
+            if(count == 0){
+                num = nums[i];
+                count++;
+            }
+            else if(num == nums[i]){
+                count++;
             }
             else{
-                hm.put(nums[i], 1);
+                count--;
             }
         }
-        for(int arr : hm.keySet()){
-            if(hm.get(arr)>nums.length/2){
-                result = arr;
-            }
-        }
-        return result;
+        return num;
     }
 }
