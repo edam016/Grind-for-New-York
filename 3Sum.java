5acfcab967c18f895f7e432ce8235a274dcfafeb
Eric Dam <edam3268@gmail.com>
2023-09-28 16:16:13 -0400

    Edit 1 LC M

--- 3Sum.java
+++ 3Sum.java
@@ -20,4 +20,33 @@ class Solution {
         }
         return new ArrayList<>(res);
     }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        HashMap<List<Integer>, Integer> hm = new HashMap();
+        List<List<Integer>> result = new ArrayList();
+        for(int i = 0;i <= nums.length-1;i++){
+            int l = i, r = nums.length-1;
+            while(l < r){
+                int sum = nums[l] + nums[r] + nums[i];
+                if(sum == 0 && l != i && i != r){
+                    List<Integer> arr = new ArrayList();
+                    arr.add(nums[l]);
+                    arr.add(nums[r]);
+                    arr.add(nums[i]);
+                    Collections.sort(arr);
+                    if(!hm.containsKey(arr)){
+                        result.add(arr);
+                        hm.put(arr, 1);
+                    }
+                }
+                if(sum > 0){
+                    r--;
+                }
+                else{
+                    l++;
+                }
+            }
+        }
+        return result;
+    }
 }
