d288982f6a659eebcd49c3ed51e9c78999caf564
Eric Dam <edam3268@gmail.com>
2023-09-28 03:39:14 -0400

    Add 1 LC M

--- RemoveDuplicatesFromSortedArray2.java
+++ RemoveDuplicatesFromSortedArray2.java
@@ -1,5 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for (int n : nums){
+        if (i < 2 || n > nums[i - 2]){
+            nums[i] = n;
+            i++;
+        }
+    }
+    return i;
+    }
+
+    public int removeDuplicates(int[] nums) {
         int val = nums[0];
         int count = 0;
         int result = nums.length;
