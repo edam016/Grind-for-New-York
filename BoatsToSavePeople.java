21ef9da1ca47a7fc6e46c3f097cee0fad72fa4da
Eric Dam <edam3268@gmail.com>
2023-09-29 22:31:06 -0400

    Edit 1 LC M

--- BoatsToSavePeople.java
+++ BoatsToSavePeople.java
@@ -1,23 +1,17 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        int l = 0, r = people.length-1, sum = 0, count = 0, left = 0;
+        int l = 0, r = people.length-1, count = 0, sum = 0;
         Arrays.sort(people);
-        while(l <= r){
+        while(l < r){
             sum = people[l] + people[r];
-            if(sum == limit){
+            if(sum > limit){
                 r--;
-                l++;
                 count++;
-            }
-            else if(sum > limit){
-                count += 1;
-                r--;
-                left = people[l];
             }
             else{
                 count++;
-                l++;
                 r--;
+                l++;
             }
         }
         return l == r ? count + 1 : count;
