5b09dd2864f1f4b95bd23cf0aea6bc595012f0b0
Eric Dam <edam3268@gmail.com>
2023-11-08 23:46:44 -0500

    Added Testing file

---
+++ IDE.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class IDE{
+    public static void main (String[] args){
+        removeKDigits("1432219", 3);
+        removeKDigits("10200", 1);
+        removeKDigits("10", 2);
+    }
+
+    public String removeKDigits(String num, int k){
+        Stack<Integer> sk = new Stack<Integer>();
+        int count = 0;
+        while(count < k){
+            sk.peek()
+            for(int i = 0;i<=num.length()-1;i++){
+                if(i == 0){
+                    sk.push(substring of value);
+                }
+                else if(!sk.isEmpty()){
+                    if(sk.peek() < substring.val){
+                        sk.pop();
+                        sk.push(substring.val);
+                    }
+                }
+            }
+        }
+    }
+}
