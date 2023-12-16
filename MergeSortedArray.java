3ab98060a5fd8e06c52cf3eafc5d410aa50ec185
Eric Dam <edam3268@gmail.com>
2023-12-16 13:46:41 -0500

    Edit LC E (TP)

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,27 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0, j = 0, k = 0;
-        if(m == 0 && n == 0){
-            return;
-        }
-        int[] res = new int[m+n];
-        while(i < m && j < n){
-            if(nums1[i] > nums2[j]){
-                res[k++] = nums2[j++];
+        int i = nums1.length-1, j = m-1, k = n-1;
+        while(i >= 0 && j >= 0 && k >= 0){
+            if(nums1[j] > nums2[k]){
+                nums1[i] = nums1[j];
+                j--;
             }
             else{
-                res[k++] = nums1[i++];
+                nums1[i] = nums2[k];
+                k--;
             }
+            i--;
         }
-        while (i < m) {
-            res[k++] = nums1[i++];
+        while(j >= 0){
+            nums1[i] = nums1[j];
+            j--;
+            i--;
         }
-
-        while (j < n) {
-            res[k++] = nums2[j++];
-        }
-        for(int b = 0;b<=res.length-1;b++){
-            nums1[b] = res[b];
+        while(k >= 0){
+            nums1[i] = nums2[k];
+            k--;
+            i--;
         }
     }
 }
