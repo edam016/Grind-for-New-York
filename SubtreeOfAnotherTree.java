6e24f614f032c53a4e808d9383c87e42a9b9e7f3
Eric Dam <edam3268@gmail.com>
2024-01-11 15:13:18 -0500

    Add 1 LC E (BST)

---
+++ SubtreeOfAnotherTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null){
+            return false;
+        }
+        if(dfs(root, subRoot)){
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean dfs(TreeNode p, TreeNode q){
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return dfs(p.left, q.left) && dfs(p.right, q.right);
+    }
+}
