68377881b2b627222e156e78321cf1a468554d4b
Eric Dam <edam3268@gmail.com>
2024-01-25 04:17:41 -0500

    Add 1 LC M (Backtrack)

---
+++ Permutation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        backtrack(res, new ArrayList(), nums);
+        return res;
+    }
+
+    public void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums){
+        if(temp.size() == nums.length){
+            res.add(new ArrayList(temp));
+        }
+        for(int i = 0;i<nums.length;i++){
+            if(temp.contains(nums[i])){
+                continue;
+            }
+            temp.add(nums[i]);
+            backtrack(res, temp, nums);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
