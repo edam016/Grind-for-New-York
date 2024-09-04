043ef5d640a6f33238950a9063a22a0a8700135d
Eric Dam <edam3268@gmail.com>
2024-09-04 19:39:05 -0400

    ADD 1 LC M

---
+++ MinimumSizeSubArray2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums){
+
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        while(right < nums.length){
+            sum += nums[right];
+            if(sum >= target){
+                minLength = Math.min(minLength, right - left + 1);
+                while(sum >= target){
+                    minLength = Math.min(minLength, right - left + 1);
+                    sum -= nums[left];
+                    left++;
+                }
+            }
+            right++;
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
