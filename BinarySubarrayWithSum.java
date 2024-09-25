36b153037433fc9ac01d2a0449bb60a84dade869
Eric Dam <edam3268@gmail.com>
2024-09-25 17:38:33 -0400

    Add 1 LC M

--- BinarySubarrayWithSum.java
+++ BinarySubarrayWithSum.java
@@ -14,3 +14,27 @@ class Solution {
         return total;
     }
 }
+
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    public int atMost(int[] nums, int goal){
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int total = 0;
+
+        while(right < nums.length){
+            sum += nums[right];
+            while(sum > goal && left <= right){
+                sum -= nums[left];
+                left++;
+            }
+            right++;
+            total += right - left + 1;
+        }
+        return total;
+    }
+}
