ece2ac15422047321208b97312fa34fd88552ae6
Eric Dam <edam3268@gmail.com>
2024-09-27 23:25:22 -0400

    Add 1 LC MD

--- FindTheDuplicateNumberBS.java
+++ FindTheDuplicateNumberBS.java
@@ -20,3 +20,23 @@ class Solution {
         return nums[left];
     }
 }
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+
+        do{
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }while(slow != fast);
+
+        slow = 0;
+
+        while(slow != fast){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+}
