4eab731a1915d866ddbb4e534bae72010570fa6d
Eric Dam <edam3268@gmail.com>
2024-10-14 17:56:19 -0400

    Add 1 LC M

---
+++ EvaluateReversePolishNotationv2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> sk = new Stack();
+        for(int i = 0;i<tokens.length;i++){
+            if(tokens[i].equals("+")){
+                int a = sk.pop();
+                int b = sk.pop();
+                sk.push(a + b);
+            }
+            else if(tokens[i].equals("-")){
+                int a = sk.pop();
+                int b = sk.pop();
+                sk.push(b - a);
+            }
+            else if(tokens[i].equals("*")){
+                int a = sk.pop();
+                int b = sk.pop();
+                sk.push(a * b);
+            }
+            else if(tokens[i].equals("/")){
+                int a = sk.pop();
+                int b = sk.pop();
+                sk.push(b / a);
+            }
+            else{
+                sk.push((Integer.parseInt(tokens[i])));
+            }
+        }
+        return sk.peek();
+    }
+}
