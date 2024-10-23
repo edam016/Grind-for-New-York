53ca61cbac58126d5665c65b67932324e7ce0cd7
Eric Dam <edam3268@gmail.com>
2024-10-22 23:39:45 -0400

    Edit

--- PostOrderTraversalv2.java
+++ PostOrderTraversalv2.java
@@ -16,7 +16,7 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList();
-        dfs(root, res);
+        dfs(root, res); 
         return res;
     }
 
