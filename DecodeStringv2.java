d3712e154ecaf3414f9bf76cdaa65c9d4a852b2b
Eric Dam <edam3268@gmail.com>
2024-10-31 00:11:09 -0400

    Add 2 Q

--- DecodeStringv2.java
+++ DecodeStringv2.java
@@ -59,3 +59,40 @@ class Solution {
         return res.toString();
     }
 }
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> sk = new Stack();
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) != ']'){
+                sk.push(s.charAt(i));
+            }
+            else{
+                StringBuilder sb = new StringBuilder();
+                while(!sk.isEmpty() && sk.peek()!= '['){
+                    sb.insert(0, sk.pop());
+                }
+                sk.pop();//remove '['
+                String word = sb.toString();
+                sb.setLength(0);
+
+                while(!sk.isEmpty() && Character.isDigit(sk.peek())){
+                    sb.insert(0, sk.pop());
+                }
+                int count = Integer.parseInt(sb.toString());
+                
+                while(count > 0){
+                    for(char ch : word.toCharArray()){
+                        sk.push(ch);
+                    }
+                    count--;
+                }
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        while(!sk.isEmpty()){
+            res.insert(0, sk.pop());
+        }
+        return res.toString();
+    }
+}
